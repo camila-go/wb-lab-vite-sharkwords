9a23977fbcd312961eca6a626ace2753370ff41c
Camila Gonzalez <camil4.gon@gmail.com>
2024-06-17 20:14:49 -0400

    sharkwords

---
+++ src/word.js
@@ -0,0 +1,24 @@
+let word;
+
+function setupWord(initWord, element) {
+  word = initWord;
+
+  word.split('').forEach(() => {
+    element.insertAdjacentHTML('beforeend', `<div class="letter-box"></div>`);
+  });
+}
+
+function isLetterInWord(letter) {
+  return word.includes(letter);
+}
+
+function revealLetterInWord(letter) {
+  const letterBoxes = document.querySelectorAll('.letter-box');
+  word.split('').forEach((wordLetter, idx) => {
+    if (wordLetter === letter) {
+      letterBoxes[idx].innerHTML = letter;
+    }
+  });
+}
+
+export { isLetterInWord, revealLetterInWord, setupWord };
